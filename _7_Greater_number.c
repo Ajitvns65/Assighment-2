3869db77585cc29e0584a5e5cd6c9084e58ef2bb
Ajitvns65 <112776228+Ajitvns65@users.noreply.github.com>
2023-04-18 04:39:09 +0530

    Add files via upload

---
+++ _7_Greater_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() 
+{ 
+    int a,b;
+    printf("Enter the value of a and b ");
+    scanf("%d %d",&a,&b);
+    printf("%d%d\n", a ,b );
+    if (a>b)
+    {
+        printf("THe vlaue of a is gratre than b\n ");
+    }
+    
+    
+    
+    
+    
+    
+    else
+    {
+        printf("The value of a and b is not equal to b ");
+    }
+    
+   
+        
+    
+        
+    
+    
+    
+    
+    return 0;
+}
